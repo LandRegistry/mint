412a55b061f00bcd42562d3212c8e6d009f7e652
treacle74 <ptrelease@gmail.com>
2015-02-06 11:15:55 +0000

    added tests for working server and helper functions

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,29 @@
-import random
 import unittest
 from application import server
+from application.server import app
+from application.server import build_system_of_record_json_string
+from application.server import return_signed_data
+
+import os
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.seq = range(10)
+        app.config.from_object(os.environ.get('SETTINGS'))
         self.app = server.app.test_client()
 
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, range(10))
+    def test_server(self):
+        self.assertEqual((self.app.get('/')).status, '200 OK')
 
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+    def test_build_system_of_record_json_string(self):
+        test_string = build_system_of_record_json_string({"a":"1"}, {"b":"2" })
+        self.assertEqual('{"sig": {"b": "2"}, "data": {"a": "1"}}', test_string)
 
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
+    def test_return_signed_data(self):
+        signed_string = return_signed_data('{"titleno": "DN1"}')
+        self.assertEqual(signed_string, "b6vjrGcLzq97_2D5h286TkRu_Kf0GonPDsndkGjhtrTBlHKIcF5H18hu635VEork_kr811ZS7B-4FuaCQFk6CvIQpNhxaMxI7m56HRQnj8ZsRSkX74xEKQUqf3k26ZdkODWJVsKyd_grJ39tfwMvJJb9V5REpRa8qXGr1eXgK4gEqwmo2fkow_W8q_yqMTTm9jOuVeFaqCQzAJBFUEWgkuTLRd91Wm8MlF4RhG_w1YktGzVath3tvaiTXNfiyfZbzPu9viotpP81gsFpWw6xocrUDbKhhXw2rm0BU2NvqSMXJ3X1qZs-VZibnWRJNNyt3sFapDojlDs99cL_uQ2aBQ")
 
-    def test_sample(self):
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
+    # test sign
+    # test verify
+    # test insert
+
