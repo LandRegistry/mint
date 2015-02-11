83c140082f0958ce70ad0b9d97d1e757b74b9b5c
ptrelease <ptrelease@gmail.com>
2015-02-11 12:13:41 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ import os
 import mock
 
 TEST_TITLE = '{"titleno": "DN1"}'
-TEST_SIGNED_TITLE = "b6vjrGcLzq97_2D5h286TkRu_Kf0GonPDsndkGjhtrTBlHKIcF5H18hu635VEork_kr811ZS7B-4FuaCQFk6CvIQpNhxaMxI7m56HRQnj8ZsRSkX74xEKQUqf3k26ZdkODWJVsKyd_grJ39tfwMvJJb9V5REpRa8qXGr1eXgK4gEqwmo2fkow_W8q_yqMTTm9jOuVeFaqCQzAJBFUEWgkuTLRd91Wm8MlF4RhG_w1YktGzVath3tvaiTXNfiyfZbzPu9viotpP81gsFpWw6xocrUDbKhhXw2rm0BU2NvqSMXJ3X1qZs-VZibnWRJNNyt3sFapDojlDs99cL_uQ2aBQ"
+TEST_SIGNATURE = "b6vjrGcLzq97_2D5h286TkRu_Kf0GonPDsndkGjhtrTBlHKIcF5H18hu635VEork_kr811ZS7B-4FuaCQFk6CvIQpNhxaMxI7m56HRQnj8ZsRSkX74xEKQUqf3k26ZdkODWJVsKyd_grJ39tfwMvJJb9V5REpRa8qXGr1eXgK4gEqwmo2fkow_W8q_yqMTTm9jOuVeFaqCQzAJBFUEWgkuTLRd91Wm8MlF4RhG_w1YktGzVath3tvaiTXNfiyfZbzPu9viotpP81gsFpWw6xocrUDbKhhXw2rm0BU2NvqSMXJ3X1qZs-VZibnWRJNNyt3sFapDojlDs99cL_uQ2aBQ"
 VERIFY_DATA = '{"sig" : "b6vjrGcLzq97_2D5h286TkRu_Kf0GonPDsndkGjhtrTBlHKIcF5H18hu635VEork_kr811ZS7B-4FuaCQFk6CvIQpNhxaMxI7m56HRQnj8ZsRSkX74xEKQUqf3k26ZdkODWJVsKyd_grJ39tfwMvJJb9V5REpRa8qXGr1eXgK4gEqwmo2fkow_W8q_yqMTTm9jOuVeFaqCQzAJBFUEWgkuTLRd91Wm8MlF4RhG_w1YktGzVath3tvaiTXNfiyfZbzPu9viotpP81gsFpWw6xocrUDbKhhXw2rm0BU2NvqSMXJ3X1qZs-VZibnWRJNNyt3sFapDojlDs99cL_uQ2aBQ", "data":{"titleno" : "DN1"}}'
 
 class TestSequenceFunctions(unittest.TestCase):
@@ -26,13 +26,13 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_return_signed_data(self):
         signed_string = return_signed_data(TEST_TITLE)
-        self.assertEqual(signed_string, TEST_SIGNED_TITLE)
+        self.assertEqual(signed_string, TEST_SIGNATURE)
 
     def test_sign_route(self):
         headers = {'content-Type': 'application/json'}
         response = self.app.post('/sign', data = TEST_TITLE, headers = headers)
         self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.data, TEST_SIGNED_TITLE)
+        self.assertEqual(response.data, TEST_SIGNATURE)
 
     def test_verify_route(self):
         headers = {'content-Type': 'application/json'}
