824ca3cab5e81b3c97f43819deadc0e13ca76185
James Lademann <james.lademann@landregsitry.gsi.gov.uk>
2015-02-25 13:48:20 +0000

    New setup for vm/deployment

--- install.sh
+++ install.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+
+virtualenv -p python2 env
+source env/bin/activate
 pip install -r requirements.txt
+
+#Set environment variable in supervisord according to deploying environment (default to development)
+case "$DEPLOY_ENVIRONMENT" in
+    development)
+		SETTINGS="config.DevelopmentConfig"
+		;;
+    test)
+		SETTINGS="config.TestConfig"
+		;;
+    production)
+		SETTINGS="config.ProductionConfig"
+		;;
+    *)
+		SETTINGS="config.DevelopmentConfig"
+		;;
+esac
+
+echo "Adding mint to supervisord..."
+cat > /etc/supervisord.d/mint.ini << EOF
+[program:mint]
+command=$dir/env/bin/gunicorn --log-file=- --log-level DEBUG -b 0.0.0.0:5000 --timeout 120 application.server:app
+directory=$dir
+autostart=true
+autorestart=true
+user=$USER
+environment=SETTINGS="$SETTINGS"
+EOF
