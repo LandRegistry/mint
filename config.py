f551ecad49ca68133eba7287fead1b24078dc335
James Lademann <james.lademann@landregsitry.gsi.gov.uk>
2015-02-25 13:47:27 +0000

    System of record endpoint in config

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ class Config(object):
     SYSTEM_OF_RECORD = os.environ['SYSTEM_OF_RECORD']
 
 class DevelopmentConfig(Config):
+    SYSTEM_OF_RECORD = 'http://10.0.2.2:5001'
     DEBUG = True
 
 class TestConfig(Config):
+    SYSTEM_OF_RECORD = 'http://127.0.0.1:5001'
     DEBUG = True
+
+class ProductionConfig(Config):
+    SYSTEM_OF_RECORD = 'http://192.168.39.5:5001'
