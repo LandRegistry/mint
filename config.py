f61beb9c5da038c9e835ed2679069d541f71370e
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2015-02-26 15:54:09 +0000

    Add configs for more environments

--- config.py
+++ config.py
@@ -2,15 +2,18 @@ import os
 
 class Config(object):
     DEBUG = False
-    SYSTEM_OF_RECORD = os.environ['SYSTEM_OF_RECORD']
+    SYSTEM_OF_RECORD = os.getenv('SYSTEM_OF_RECORD', 'http://127.0.0.1:5001']
 
 class DevelopmentConfig(Config):
-    SYSTEM_OF_RECORD = 'http://10.0.2.2:5001'
-    DEBUG = True
-
-class TestConfig(Config):
     SYSTEM_OF_RECORD = 'http://127.0.0.1:5001'
     DEBUG = True
+
+class PreviewConfig(Config):
+    SYSTEM_OF_RECORD = 'http://127.0.0.1:5001'
+    DEBUG = True
+
+class PreProductionConfig(Config):
+    SYSTEM_OF_RECORD = 'http://192.168.39.5:5001'
 
 class ProductionConfig(Config):
     SYSTEM_OF_RECORD = 'http://192.168.39.5:5001'
