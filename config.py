6dd4521e65db18063158d4d006b655fe0277a15f
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2015-02-26 15:57:01 +0000

    Typo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     DEBUG = False
-    SYSTEM_OF_RECORD = os.getenv('SYSTEM_OF_RECORD', 'http://127.0.0.1:5001']
+    SYSTEM_OF_RECORD = os.getenv('SYSTEM_OF_RECORD', 'http://127.0.0.1:5001')
 
 class DevelopmentConfig(Config):
     SYSTEM_OF_RECORD = 'http://127.0.0.1:5001'
