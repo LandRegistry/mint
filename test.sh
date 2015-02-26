1730e4dc3d8c2323491968e0aee03b36789a057d
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2015-02-26 17:48:07 +0000

    activate virtualenv

--- test.sh
+++ test.sh
@@ -3,5 +3,7 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
 
+source ~/venvs/mint/bin/activate
+
 source ./environment-test.sh
 py.test --junitxml=TEST-mint.xml --cov application tests
