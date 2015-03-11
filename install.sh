e94ceaf095b78f11cd922f67d08a26f6de75a173
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2015-03-11 17:10:15 +0000

    Only add directory if not there

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ source ~/venvs/mint/bin/activate
 pip install -r requirements.txt
 
 #Create the logging directory as it is required by default
-mkdir $dir/logs
+if [ ! -d $dir/logs ]; then
+	mkdir $dir/logs
+fi
 
 #Set environment variable in supervisord according to deploying environment (default to development)
 case "$DEPLOY_ENVIRONMENT" in
