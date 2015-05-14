d921d5684c533d6828f550b958cc3606e8e8e611
James Lademann <james.lademann@landregistry.gsi.gov.uk>
2015-05-14 12:08:36 +0100

    Split install into install and post-install .sh

---
+++ post-install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+
+#Set environment variable in supervisord according to deploying environment (default to development)
+case "$DEPLOY_ENVIRONMENT" in
+    development)
+		SUPERVISOR_ENV="SETTINGS=\"config.DevelopmentConfig\""
+		COMMAND="$HOME/venvs/mint/bin/python run.py"
+		;;
+    preview)
+		SUPERVISOR_ENV="SETTINGS=\"config.PreviewConfig\""
+		COMMAND="$HOME/venvs/mint/bin/python run.py"
+		;;
+	release)
+		SUPERVISOR_ENV="SETTINGS=\"config.ReleaseConfig\""
+		COMMAND="$HOME/venvs/mint/bin/python run.py"
+		;;
+    preproduction)
+		SUPERVISOR_ENV="SETTINGS=\"config.PreProductionConfig\""
+		COMMAND="$HOME/venvs/mint/bin/gunicorn -w 16 --log-file=- --log-level DEBUG -b 0.0.0.0:5000 --timeout 120 application.server:app"
+		;;
+    production)
+		SUPERVISOR_ENV="SETTINGS=\"config.ProductionConfig\""
+		COMMAND="$HOME/venvs/mint/bin/gunicorn -w 16 --log-file=- --log-level DEBUG -b 0.0.0.0:5000 --timeout 120 application.server:app"
+		;;
+    *)
+		SUPERVISOR_ENV="SETTINGS=\"config.DevelopmentConfig\""
+		COMMAND="$HOME/venvs/mint/bin/python run.py"
+		;;
+esac
+
+if [ -n "$SYSTEM_OF_RECORD" ]; then
+	SUPERVISOR_ENV="$SUPERVISOR_ENV,SYSTEM_OF_RECORD=\"$SYSTEM_OF_RECORD\""
+fi
+
+echo "Adding mint to supervisord..."
+cat > /etc/supervisord.d/mint.ini << EOF
+[program:mint]
+command=$COMMAND
+directory=$dir
+autostart=true
+autorestart=true
+user=$USER
+environment=$SUPERVISOR_ENV
+stopasgroup=true
+EOF
